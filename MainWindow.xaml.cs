45a6659a674991b3a88c602c49f390e47cba06dc
mslowin <slowinski.michal@outlook.com>
2022-10-26 22:42:11 +0200

    Dodano wstępną obsługę komend lini poleceń (synchroniczne wywołania multichain-cli). Zastanowić się nad komendą multichaind.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LucidOcean.MultiChain;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,8 +24,34 @@ namespace GameBlocks
     {
         public MainWindow()
         {
-            // HELLO WORLD
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Na razie chaina trzeba najpierw uruchomić ręcznie;
+            string output, err;
+            (output, err) = runCommand("liststreams");
+            textBox1.Text = output;
+        }
+
+        public (string, string) runCommand(string command)
+        {
+            Process process = new Process();
+            process.StartInfo.WorkingDirectory = @"d:\Multichain\";
+            process.StartInfo.FileName = @"d:\Multichain\multichain-cli.exe";
+            process.StartInfo.Arguments = $"chain11 {command}";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.Start();
+            //* Read the output (or the error)
+            string output = process.StandardOutput.ReadToEnd();
+            string err = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+
+            return (output, err);
         }
     }
 }
