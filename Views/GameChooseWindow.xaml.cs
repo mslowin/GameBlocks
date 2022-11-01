d4b7448e5682220489d9c026cbe4d427efe83c9e
mslowin <slowinski.michal@outlook.com>
2022-11-01 18:33:55 +0100

    Added GameChooseWindow Layout.

--- Views/GameChooseWindow.xaml.cs
+++ Views/GameChooseWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameBlocks.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WPFCustomMessageBox;
 
 namespace GameBlocks.Views
 {
@@ -19,9 +21,47 @@ namespace GameBlocks.Views
     /// </summary>
     public partial class GameChooseWindow : Window
     {
+        /// <summary>
+        /// GameChooseWindow constructor.
+        /// </summary>
         public GameChooseWindow()
         {
             InitializeComponent();
+        }
+
+        private void Left_Arrow_Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Method intentionally left empty.
+        }
+
+        private void Right_Arrow_Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Method intentionally left empty.
+        }
+
+        /// <summary>
+        /// Handling the window closing event.
+        /// </summary>
+        /// <param name="sender">Reference to the sender e.g. button.</param>
+        /// <param name="e">Additional information object and event handler.</param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var result = CustomMessageBox.Show("Do you really want to exit?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result != MessageBoxResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        /// <summary>
+        /// Handling an instance where the window was closed.
+        /// </summary>
+        /// <param name="sender">Reference to the sender e.g. button.</param>
+        /// <param name="e">Additional information object and event handler.</param>
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            // Stopping a Node
+            MultiChainClient.RunCommand("multichain-cli", GlobalVariables.ChainName, "stop");
         }
     }
 }
